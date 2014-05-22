38dbc33f5f784c0f2dea37c4ad8d9a7d6ec19c95
Adam Morgan <atom.morgan@gmail.com>
2014-05-22 16:03:13 +0000

    Add triangle project solution

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  count = [ a == b, b == c, a == c ].select { |sides| sides }
+  case count.size 
+    when 0
+      :scalene
+    when 1
+      :isosceles
+    when 3
+      :equilateral
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
